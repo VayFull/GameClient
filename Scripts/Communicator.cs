f1e50364fe64fd1085de3ba5b0b26b9d4679bdd7
igor <kv4snikov@gmail.com>
2021-01-04 15:12:35 +0300

    init

---
+++ Scripts/Communicator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class Communicator : MonoBehaviour
+{
+    private UdpClient _udpClient;
+    private IPEndPoint _serverEndpoint;
+    public int Id;
+    
+    void Start()
+    {
+        _udpClient = new UdpClient();
+    }
+    
+    public void JoinServer(string hostname, int port)
+    {
+        _serverEndpoint = new IPEndPoint(IPAddress.Parse(hostname), port);
+        _udpClient.Connect(hostname, port);
+        _udpClient.BeginReceive(ReceiveCallback, null);
+        
+        SendHelloPackage();
+    }
+
+    private void ReceiveCallback(IAsyncResult ar)
+    {
+        var receivedBytes = _udpClient.EndReceive(ar, ref _serverEndpoint);
+        var result = Encoding.ASCII.GetString(receivedBytes);
+        if (result.StartsWith("id:"))
+        {
+            var resultParts = result.Split(':');
+            Id = Int32.Parse(resultParts[1]);
+        }
+        Debug.Log(result);
+        _udpClient.BeginReceive(ReceiveCallback, null);
+    }
+
+    public void SendHelloPackage()
+    {
+        var bytes = Encoding.ASCII.GetBytes("hello");
+        _udpClient.BeginSend(bytes, bytes.Length, SendCallback, null);
+    }
+
+    private void SendCallback(IAsyncResult ar)
+    {
+        _udpClient.EndSend(ar);
+    }
+}
---
+++ Scripts/UiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiController : MonoBehaviour
+{
+    public InputField HostnameInput;
+    public InputField PortInput;
+
+    public GameObject CommunicatorGameObject;
+    private Communicator Communicator;
+    
+    private void Start()
+    {
+        Communicator = CommunicatorGameObject.GetComponent<Communicator>();
+    }
+
+    public void TryToConnect()
+    {
+        var hostnameValue = HostnameInput.text;
+        var portValue = Int32.Parse(PortInput.text);
+        
+        Communicator.JoinServer(hostnameValue, portValue);
+    }
+}
